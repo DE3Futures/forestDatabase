843cbeae1bccbe48a1154428c40387ac101e1007
Hannah Jayne Knight <hannah@tinpot.co.uk>
2022-02-06 12:10:40 +0000

    Setting up cookiecutter datascience structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DE3 Futures module',
+    author='Team 14, DE3',
+    license='',
+)
